29c5861bee6f25cdc0f0e80394ba6cc041aa053f
tuofan <zenglu0731@qq.com>
2018-05-22 19:54:39 +0800

    test commit
    Signed-off-by: tuofan <zenglu0731@qq.com>

---
+++ shiro-modules/src/test/java/com/shiro/test/AuthenticationTest.java
@@ -0,0 +1,37 @@
+package com.shiro.test;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.realm.SimpleAccountRealm;
+import org.apache.shiro.subject.Subject;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by tuoFan on 2018-5-22.
+ * shiro 极简测试类
+ */
+public class AuthenticationTest {
+
+    SimpleAccountRealm simpleAccountRealm = new SimpleAccountRealm();
+
+    @Before
+    public void addUser(){
+        simpleAccountRealm.addAccount("tuofan","admin123");
+    }
+
+    @Test
+    public void testAuthentication(){
+
+        // 1.构建SecurityManager环境
+        DefaultSecurityManager defaultSecurityManager = new DefaultSecurityManager();
+        defaultSecurityManager.setRealm(simpleAccountRealm);
+        // 2.主体提交认证请求
+        SecurityUtils.setSecurityManager(defaultSecurityManager);
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("tuofan","admin123");
+        subject.login(token);
+        System.out.print("subject.isAuthenticated():"+subject.isAuthenticated());
+    }
+}
